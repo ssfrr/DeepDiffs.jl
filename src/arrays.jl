1e27f5ed0e1cd9bae990ac81a0401df2cd89a4a3
Gustavo Goretkin <gustavo.goretkin@gmail.com>
2020-05-10 00:11:59 -0400

    Baby step

--- src/arrays.jl
+++ src/arrays.jl
@@ -23,36 +23,37 @@ function deepdiff(X::Vector, Y::Vector)
     # substrings.
 
     lengths = zeros(Int, length(X)+1, length(Y)+1)
-    backtracks = fill(:nothing, axes(lengths))
+    backtracks = fill(:nothing, length(X), length(Y))
 
     for (j, v2) in enumerate(Y)
         for (i, v1) in enumerate(X)
             if isequal(v1, v2)
                 lengths[i+1, j+1] = lengths[i, j] + 1
-                backtracks[i+1, j+1] = :XY
+                backtracks[i, j] = :XY
             else
-                (lengths[i+1, j+1], backtracks[i+1, j+1]) = _argmax(lengths[i+1, j], lengths[i, j+1])
+                (lengths[i+1, j+1], backtracks[i, j]) = _argmax(lengths[i+1, j], lengths[i, j+1])
             end
         end
     end
 
     removed = Int[]
     added = Int[]
-    backtrack(lengths, removed, added, X, Y, length(X), length(Y))
+
+    backtrack(lengths, backtracks, removed, added, X, Y, length(X), length(Y))
 
     VectorDiff(X, Y, removed, added)
 end
 
 # recursively trace back the longest common subsequence, adding items
 # to the added and removed lists as we go
-function backtrack(lengths, removed, added, X, Y, i, j)
-    if i > 0 && j > 0 && isequal(X[i], Y[j])
-        backtrack(lengths, removed, added, X, Y, i-1, j-1)
-    elseif j > 0 && (i == 0 || lengths[i+1, j] ≥ lengths[i, j+1])
-        backtrack(lengths, removed, added, X, Y, i, j-1)
+function backtrack(lengths, backtracks, removed, added, X, Y, i, j)
+    if i > 0 && j > 0 && backtracks[i, j] == :XY
+        backtrack(lengths, backtracks, removed, added, X, Y, i-1, j-1)
+    elseif j > 0 && (i == 0 || backtracks[i, j] == :X)
+        backtrack(lengths, backtracks, removed, added, X, Y, i, j-1)
         push!(added, j)
-    elseif i > 0 && (j == 0 || lengths[i+1, j] < lengths[i, j+1])
-        backtrack(lengths, removed, added, X, Y, i-1, j)
+    elseif i > 0 && (j == 0 ||  backtracks[i, j] == :Y)
+        backtrack(lengths, backtracks, removed, added, X, Y, i-1, j)
         push!(removed, i)
     end
 end
